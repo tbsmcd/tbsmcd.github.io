fbab6d26019b426cf06b62ae49ef3779618d92ff
tbsmcd <tbsmcd@gmail.com>
2020-03-02 22:23:06 +0900

    script を追加

--- content/search.md
+++ content/search.md
@@ -3,7 +3,17 @@ title: "Search"
 draft: false
 ---
 {{< raw >}}
-<script >
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.28/vue.min.js"></script>
+<script src="/js/lunr.js"></script>
+<script src="/js/lunr.stemmer.support.js"></script>
+<script src="/js/lunr.ja.js"></script>
+<script src="/js/lunr.multi.js"></script>
+<script src="/js/search.js"></script>
 
+<div id="app">
+  <mysearchbtn :search.sync="search"></mysearchbtn>
+  <p>Number of results: {{list.length}}</p>
+  <mylist :list="list"></mylist>
+</div>
 
 {{< /raw >}}
---
+++ static/js/search.js
@@ -0,0 +1,64 @@
+Vue.component('mysearchbtn', {
+  template:`<div>
+    <input type="text" placeholder="type to search"
+      v-model="search"
+      @input="$emit('update:search', $event.target.value)" />
+</div>`,
+  props:['search']
+})
+
+
+Vue.component('mylist', {
+  template:`<ul>
+    <li v-for="item in list" :key="item.id">{{item.name}}</li>
+</ul>`,
+  props:['list']
+})
+
+
+new Vue({
+  el:'#app',
+  data(){
+    return{
+       original:[],
+       list:[],
+       search:'',
+       resuls:[],
+       searchIndex:null
+    }
+  },
+  mounted(){
+    axios('https://tbsmcd.net/index.json')
+       .then(data => {
+          this.original = data.data
+          this.list = this.original
+          this.buildIndex()
+      })
+    
+    this.$watch('search', () => {
+      this.resuls = this.searchIndex.search(this.search)
+      
+      this.list = []
+      this.resuls.forEach(d => {
+        this.original.forEach(p => {
+          if(d.ref == p.id) this.list.push(p)
+        })
+        
+      })
+    })
+  },
+  methods:{
+    buildIndex(){
+      var documents = this.original
+      this.searchIndex = lunr(function () {
+	this.use(lunr.multiLanguage('en', 'ja'))
+        this.field('body')
+        this.field('title')
+
+        documents.forEach(doc => {
+          this.add(doc)
+        })
+      })
+    }
+  }
+})
