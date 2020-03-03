ea5ab17de8b229147be811ab164b0722c8260bca
tbsmcd <tbsmcd@gmail.com>
2020-03-03 22:56:17 +0900

    script 内で読み込み

--- content/search.md
+++ content/search.md
@@ -11,11 +11,14 @@ draft: false
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+<!--
 <script src="/js/lunr.js"></script>
 <script src="/js/lunr.stemmer.support.js"></script>
 <script src="/js/tinyseg.js"></script>
 <script src="/js/lunr.ja.js"></script>
 <script src="/js/lunr.multi.js"></script>
+
+-->
 <script src="/js/search.js"></script>
 
 {{< /raw >}}
--- static/js/search.js
+++ static/js/search.js
@@ -1,66 +1,69 @@
 Vue.component('mysearchbtn', {
-  template:`<div>
+	template:`<div>
     <input type="text" placeholder="type to search"
       v-model="search"
       @input="$emit('update:search', $event.target.value)" />
 </div>`,
-  props:['search']
+	props:['search']
 })
 
 
 Vue.component('mylist', {
-  template:`<ul>
+	template:`<ul>
     <li v-for="item in list">{{item.title}}</li>
 </ul>`,
-  props:['list']
+	props:['list']
 })
 
 
 new Vue({
-  el:'#app',
-  data(){
-    return{
-       original:[],
-       list:[],
-       search:'',
-       resuls:[],
-       searchIndex:null
-    }
-  },
-  mounted(){
-    axios.get('https://tbsmcd.net/index.json')
-       .then(response => {
-          this.original = response.data
-          this.list = this.original
-          this.buildIndex()
-      })
-    
-    this.$watch('search', () => {
-      this.resuls = this.searchIndex.search(this.search)
-      
-      this.list = []
-      this.resuls.forEach(d => {
-        this.original.forEach(p => {
-          if(d.ref == p.id) this.list.push(p)
-        })
-        
-      })
-    })
-  },
-  methods:{
-    buildIndex(){
-      var documents = this.original
-      this.searchIndex = lunr(function () {
-        //this.use(lunr.multiLanguage('en', 'ja'))
-        this.use(lunr.ja)
-	this.ref('id')
-        this.field('body')
-        this.field('title')
+	el:'#app',
+	data(){
+		return{
+			original:[],
+			list:[],
+			search:'',
+			resuls:[],
+			searchIndex:null
+		}
+	},
+	mounted(){
+		axios.get('https://tbsmcd.net/index.json')
+			.then(response => {
+				this.original = response.data
+				this.list = this.original
+				this.buildIndex()
+			})
 
-        documents.forEach(doc => {
-          this.add(doc)
-        })
-      })
-    }
-  }
+		this.$watch('search', () => {
+			this.resuls = this.searchIndex.search(this.search)
+
+			this.list = []
+			this.resuls.forEach(d => {
+				this.original.forEach(p => {
+					if(d.ref == p.id) this.list.push(p)
+				})
+
+			})
+		})
+	},
+	methods:{
+		buildIndex(){
+			var documents = this.original
+			var lunr = require('./lunr.js')
+			require('./lunr.stemmer.support.js')(lunr)
+			require('./lunr.ja.js')(lunr)
+			require('./lunr.multi.js')(lunr)
+			this.searchIndex = lunr(function () {
+				this.use(lunr.multiLanguage('en', 'ja'))
+				this.ref('id')
+				this.field('body')
+				this.field('title')
+
+				documents.forEach(doc => {
+					this.add(doc)
+				})
+			})
+		}
+	}
 })
