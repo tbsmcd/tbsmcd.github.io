69f40288b2ba156330ef9842c14708d6a49302f8
tbsmcd <tbsmcd@gmail.com>
2020-07-08 23:56:58 +0900

    記事作成スクリプト修正2

--- create.py
+++ create.py
@@ -8,36 +8,39 @@ def input_dir_name():
     name = input('Directory name: ')
     path = os.path.dirname(__file__) + '/content/post/{0}'.format(name)
     if os.path.exists(path):
-        print('This dir_name exists. Please use other name.')
+        edit = input('This dir_name exists. Edit this page?(Y/n) : ')
+        if edit == 'y' or edit == 'Y':
+            return name, False
+        print('Please use other name.')
         input_dir_name()
     else:
-        return name
+        return name, True
 
 
 try:
-    print('Create new page.')
-    dir_name = input_dir_name()
-    title = input('Title: ')
-    desc = input('Description: ')
-    img = input('OGP image: ')
-    tags = input('Tags(comma separated): ')
-    draft = input('Draft(Y/n): ')
-
+    dir_name, is_new = input_dir_name()
     dir_path = os.path.dirname(__file__) + '/content/post/{0}'.format(dir_name)
-    print('Creating page: {0}/index.md'.format(dir_path))
+    if is_new is True:
+        print('Create new page.')
+        title = input('Title: ')
+        desc = input('Description: ')
+        img = input('OGP image: ')
+        tags = input('Tags(comma separated): ')
+        draft = input('Draft(Y/n): ')
+        print('Creating page: {0}/index.md'.format(dir_path))
 
-    # タグを整理する
-    tags_list = ['"{0}"'.format(x.strip()) for x in tags.split(',')]
-    tags_str = ', '.join(tags_list)
+        # タグを整理する
+        tags_list = ['"{0}"'.format(x.strip()) for x in tags.split(',')]
+        tags_str = ', '.join(tags_list)
 
-    # is draft
-    if draft == 'y' or draft == 'Y':
-        is_draft = 'true'
-    else:
-        is_draft = 'false'
+        # is draft
+        if draft == 'y' or draft == 'Y':
+            is_draft = 'true'
+        else:
+            is_draft = 'false'
 
-    dt_now = datetime.datetime.now()
-    header = """---
+        dt_now = datetime.datetime.now()
+        header = """---
 title: "{0}"
 description: "{1}"
 image: "{2}"
@@ -45,18 +48,20 @@ date: {3}
 tags: [{4}]
 archives: ["{5}"]
 draft: {6}
-    ---
-    """.format(title, desc, img, dt_now.strftime('%Y-%m-%dT%H:%M:%S+09:00'),
-               tags_str, dt_now.strftime('%Y-%m'), is_draft)
+---
+        """.format(title, desc, img, dt_now.strftime('%Y-%m-%dT%H:%M:%S+09:00'),
+                   tags_str, dt_now.strftime('%Y-%m'), is_draft)
 
-    # ファイル作成
-    os.makedirs(dir_path, exist_ok=True)
-    with open(dir_path + '/index.md', mode='w') as f:
-        f.write(header)
-
+        # ファイル作成
+        os.makedirs(dir_path, exist_ok=True)
+        with open(dir_path + '/index.md', mode='w') as f:
+            f.write(header)
+    else:
+        print('Edit page.')
     # 規定のエディタを取得して編集
     EDITOR = os.environ.get('EDITOR', 'vim')
     run([EDITOR, dir_path + '/index.md'])
     run(['open', dir_path])
+    print('Finished.')
 except KeyboardInterrupt:
     print('\nStopped(Keyboard Interrupt).')
