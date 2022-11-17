5557bc9d389aa314d4813a533e5a1e9b8325ecf7
tbsmcd <tbsmcd@gmail.com>
2022-11-17 23:47:28 +0900

    fix js,css

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,12 +1,21 @@
-window.addEventListener('load', () => {
-    const selectedTheme = this.localStorage.getItem('selectedTheme');
-    if (selectedTheme == 'dark') {
+const cbDark = document.getElementById("cb-dark-theme");
+const setDark = () => {
+    document.body.classList.add("dark-theme");
+    cbDark.checked = true;
+};
+const setLight = () => {
+    document.body.classList.remove("dark-theme");
+    cbDark.checked = false;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const selectedTheme = localStorage.getItem("selectedTheme");
+    if (selectedTheme == "dark") {
         setDark();
-    } else if (selectedTheme == 'light') {
+    } else if (selectedTheme == "light") {
         setLight();
     } else {
-        const mql = this.matchMedia('(prefers-color-scheme: dark)');
-        if (mql.matches) {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
             setDark();
         } else {
             setLight();
@@ -14,11 +23,11 @@ window.addEventListener('load', () => {
     }
 });
 
-const setDark = () => {
-    document.body.classList.remove('light-theme');
-    document.body.classList.add('dark-theme');
-};
-const setLight = () => {
-    document.body.classList.remove('dark-theme');
-    document.body.classList.add('light-theme');
-}
+cbDark.addEventListener("change", () => {
+    if (this.checked == true) {
+        setDark();
+        localStorage.setItem("selectedTheme", "dark");
+    } else {
+        localStorage.setItem("selectedTheme", "light");
+    }
+});
