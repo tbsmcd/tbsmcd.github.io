c6a34c68cf599c8106948804e80b4e0f8f908032
tbsmcd <tbsmcd@users.noreply.github.com>
2022-11-20 13:25:49 +0000

    deploy: f5708ff42dcea1fdd8b34b4a5c179e1297c5e259

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const selectedTheme = localStorage.getItem("selectedTheme");
-    console.log("localStorage");
-    console.log(selectedTheme);
     if (selectedTheme == "dark") {
         setDark();
     } else if (selectedTheme == "light") {
@@ -17,17 +15,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 const cbDark = document.getElementById("cb-dark-theme");
 const setDark = () => {
-    console.log("DARK");
     document.body.classList.add("dark-theme");
     cbDark.checked = true;
 };
 const setLight = () => {
-    console.log("LIGHT");
     document.body.classList.remove("dark-theme");
     cbDark.checked = false;
 }
 cbDark.addEventListener("change", (e) => {
-    console.log("cliekd");
     if (e.target.checked) {
         console.log("cheked");
         setDark();
